201345e7b20000ebbf0f0ceacae35547b76a8edb
LeandroBarreiro <leandrobarreiro15@gmail.com>
2025-07-04 06:51:41 -0400

    modified:   modulo-02/aula-tipo-any/anyType.js 	new file:   modulo-02/aula-tipo-unknown/unknownType.js 	new file:   modulo-02/aula-tipo-unknown/unknownType.ts

--- modulo-02/aula-tipo-any/anyType.js
+++ modulo-02/aula-tipo-any/anyType.js
@@ -3,8 +3,14 @@
 const a = 888;
 const b = ['Leandro'];
 const result = a + b;
-console.log(result);
+console.log(result); //Apenas concatena duas varias 
 //Exemplo 02: Quando o tipo 'Any' é inferido implicitamente
 let frase;
-frase = 'Oi pessoal! Tudo bem?';
+frase = 'Oi pessoal! Tudo bem?'; //Essa variavel é do tipo Any
 console.log(frase);
+//Exemplo 03: Quando devemos usar o tipo any?
+const formulario = {
+    nome: 'Leandro',
+    sobrenome: 'Barreiro',
+    idade: 19,
+};
---
+++ modulo-02/aula-tipo-unknown/unknownType.js
@@ -0,0 +1,16 @@
+"use strict";
+//Exemplo 01: Tipo Unknown
+let valorVariavel;
+valorVariavel = true;
+valorVariavel = 123;
+valorVariavel = [];
+valorVariavel = 'oi! tudo bem?';
+console.log(valorVariavel);
+//Exemplo 02: Erro ao tentar atribuir um valor do tipo 'unknown' a outros tipos!
+// let valor: unknown;
+// let valor1: boolean = valor;
+//Exemplo 03: Diferença entre 'any' vs 'Unknown'
+let algumaCoisaAny;
+let algumaCoisaUnknown;
+console.log(algumaCoisaAny.toFixed(2));
+//console.log(algumaCoisaUnknown.toFixed(2));
